cb2eb1790024e09e1524cccc1625a1016e8e82f3
Carlos Moreira <1161882@isep.ipp.pt>
2019-11-15 16:30:15 +0000

    e2e Machines and Operations Tests [ref issue #7] [ref issue #12]

--- myOwnCutleryWebApp/e2e/src/app.e2e-spec.ts
+++ myOwnCutleryWebApp/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,76 @@
-import { AppPage } from './app.po';
+import { MachinesPage, OperationsPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+// ***************
+// MACHINES
+// ***************
+describe('MachinesPage App', () => {
+  let page: MachinesPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new MachinesPage();
   });
 
-  it('should display welcome message', () => {
+  it('should have Id in table header', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to myOwnCutleryWebApp!');
+    expect(page.getMachinesId()).toEqual('Id');
+  });
+
+  it('should have Description in table header', () => {
+    page.navigateTo();
+    expect(page.getMachinesDescription()).toEqual('Description');
+  });
+
+  it('should have new button', () => {
+    page.navigateTo();
+    browser.pause();
+    expect(page.getNewButton().getText()).toContain('New');
+  });
+
+  it('should have edit button', () => {
+    page.navigateTo();
+    browser.pause();
+    expect(page.getEditButton()).toContain('Edit');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
+
+// ***************
+// OPERATIONS
+// ***************
+describe('Operations App', () => {
+  let page: OperationsPage;
+
+  beforeEach(() => {
+    page = new OperationsPage();
+  });
+
+  it('should have Id in table header', () => {
+    page.navigateTo();
+    expect(page.getOperationsId()).toEqual('Id');
+  });
+
+  it('should have Tool in table header', () => {
+    page.navigateTo();
+    expect(page.getOperationsTool()).toEqual('Tool');
+  });
+
+  it('should have OperationType in table header', () => {
+    page.navigateTo();
+    expect(page.getOperationsOperationType()).toEqual('Operation Type');
+  });
+
+  it('should have new button', () => {
+    page.navigateTo();
+    browser.pause();
+    expect(page.getNewButton().getText()).toContain('New');
   });
 
   afterEach(async () => {
--- myOwnCutleryWebApp/e2e/src/app.po.ts
+++ myOwnCutleryWebApp/e2e/src/app.po.ts
@@ -9,3 +9,57 @@ export class AppPage {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
+
+// ***************
+// MACHINES
+// ***************
+export class MachinesPage {
+
+  navigateTo() {
+    return browser.get('http://localhost:4200/machines');
+  }
+
+  getMachinesId() {
+    return element(by.xpath(`//span[1][contains(@class, 'mat-header-cell')]`)).getText();
+  }
+
+  getMachinesDescription() {
+    return element(by.xpath(`//span[2][contains(@class, 'mat-header-cell')]`)).getText();
+  }
+
+  getNewButton() {
+    return element(by.cssContainingText('button', 'New'));
+  }
+
+  getEditButton() {
+    return element(by.xpath(` //button[contains(@class,'btn btn-primary buttonTables')]`)).getText();
+  }
+
+}
+
+// ***************
+// OPERATIONS
+// ***************
+export class OperationsPage {
+
+  navigateTo() {
+    return browser.get('http://localhost:4200/operations');
+  }
+
+  getOperationsId() {
+    return element(by.xpath(`//span[1][contains(@class, 'mat-header-cell')]`)).getText();
+  }
+
+  getOperationsTool() {
+    return element(by.xpath(`//span[2][contains(@class, 'mat-header-cell')]`)).getText();
+  }
+
+  getOperationsOperationType() {
+    return element(by.xpath(`//span[3][contains(@class, 'mat-header-cell')]`)).getText();
+  }
+
+  getNewButton() {
+    return element(by.cssContainingText('button', 'New'));
+  }
+
+}
