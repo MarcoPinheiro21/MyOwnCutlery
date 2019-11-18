e8075a8f9c5df73e302dda4561f0f99842293d4f
Carlos Moreira <1161882@isep.ipp.pt>
2019-11-18 22:04:20 +0000

    LAPR5 - Avaliação Pares [ref issue #47]

--- bashScript.sh
+++ bashScript.sh
@@ -23,3 +23,10 @@ tell application "Terminal"
     delay 0.25
     do script "cd myOwnCutleryWebApp && ng serve" in front window
 end tell'
+
+osascript -e 'tell application "Terminal" to activate
+tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
+tell application "Terminal"
+    delay 0.25
+    do script "cd visualization && npx serve -l 6001" in front window
+end tell'
