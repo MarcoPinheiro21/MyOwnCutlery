8665490beb3063877c82641ec695b2221f57e2d2
Carlos Moreira <1161882@isep.ipp.pt>
2019-12-10 21:47:46 +0000

    ARQSI_49_MA - Alterar serviço de validação [ref issue #96]

--- authApi/routes/users.js
+++ authApi/routes/users.js
@@ -4,6 +4,9 @@ var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var UserModel = require("../models/User");
 var VerifyToken = require("../auth/VerifyToken");
+const fs = require('fs');
+
+var rolesData = require('../config/roles.json');
 
 var router = express.Router();
 
@@ -28,8 +31,30 @@ router.get("/", function(req, res) {
           .status(400)
           .send({ auth: false, token: null, message: "Username not found with that token!" });
       } else if (user) {
+
+        let privileges;
+
+        switch (user.role) {
+          case 'client':
+            privileges = rolesData.roles.client;
+            break;
+          case 'clientConfiguration':
+            privileges = rolesData.roles.clientConfiguration;
+            break;
+          case 'dataAdmin':
+              privileges = rolesData.roles.dataAdmin;
+            break;
+          case 'productionManager':
+              privileges = rolesData.roles.productionManager;
+            break;
+          case 'admin':
+              privileges = rolesData.roles.admin;
+            break;
+        }
+          
         res.json({
-          role: user.role
+          role: user.role,
+          privileges: privileges
         });
       }
     });
@@ -90,6 +115,13 @@ router.post("/login", function(req, res) {
       }
     }
   });
+});
+
+fs.watchFile(require.resolve('../config/roles.json'), function () {
+  console.log("Reloading roles file....");
+  delete require.cache[require.resolve('../config/roles.json')]
+  console.log("Reloaded completed!");
+  rolesData = require('../config/roles.json');
 });
 
 module.exports = router;
