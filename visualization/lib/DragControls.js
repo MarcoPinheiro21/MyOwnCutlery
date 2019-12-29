8428d6141b82e1c14f343d509382e539129a211f
Bruno Alves <brunofigalves@Brunos-MBP.lan>
2019-12-29 21:21:51 +0000

    SGRAI - melhoria de drag and drop nos objectos tipo group

--- visualization/lib/DragControls.js
+++ visualization/lib/DragControls.js
@@ -1,10 +1,11 @@
 /**
  * @author zz85 / https://github.com/zz85
  * @author mrdoob / http://mrdoob.com
+ * @adapted brunofigalves
  * Running this will allow you to drag three.js objects around the screen.
  */
 
-THREE.DragControls = function ( _objects, _camera, _domElement ) {
+THREE.DragControls = function (_objects, _camera, _domElement) {
 
 	var _plane = new THREE.Plane();
 	var _raycaster = new THREE.Raycaster();
@@ -23,25 +24,25 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 	function activate() {
 
-		_domElement.addEventListener( 'mousemove', onDocumentMouseMove, false );
-		_domElement.addEventListener( 'mousedown', onDocumentMouseDown, false );
-		_domElement.addEventListener( 'mouseup', onDocumentMouseCancel, false );
-		_domElement.addEventListener( 'mouseleave', onDocumentMouseCancel, false );
-		_domElement.addEventListener( 'touchmove', onDocumentTouchMove, false );
-		_domElement.addEventListener( 'touchstart', onDocumentTouchStart, false );
-		_domElement.addEventListener( 'touchend', onDocumentTouchEnd, false );
+		_domElement.addEventListener('mousemove', onDocumentMouseMove, false);
+		_domElement.addEventListener('mousedown', onDocumentMouseDown, false);
+		_domElement.addEventListener('mouseup', onDocumentMouseCancel, false);
+		_domElement.addEventListener('mouseleave', onDocumentMouseCancel, false);
+		_domElement.addEventListener('touchmove', onDocumentTouchMove, false);
+		_domElement.addEventListener('touchstart', onDocumentTouchStart, false);
+		_domElement.addEventListener('touchend', onDocumentTouchEnd, false);
 
 	}
 
 	function deactivate() {
 
-		_domElement.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-		_domElement.removeEventListener( 'mousedown', onDocumentMouseDown, false );
-		_domElement.removeEventListener( 'mouseup', onDocumentMouseCancel, false );
-		_domElement.removeEventListener( 'mouseleave', onDocumentMouseCancel, false );
-		_domElement.removeEventListener( 'touchmove', onDocumentTouchMove, false );
-		_domElement.removeEventListener( 'touchstart', onDocumentTouchStart, false );
-		_domElement.removeEventListener( 'touchend', onDocumentTouchEnd, false );
+		_domElement.removeEventListener('mousemove', onDocumentMouseMove, false);
+		_domElement.removeEventListener('mousedown', onDocumentMouseDown, false);
+		_domElement.removeEventListener('mouseup', onDocumentMouseCancel, false);
+		_domElement.removeEventListener('mouseleave', onDocumentMouseCancel, false);
+		_domElement.removeEventListener('touchmove', onDocumentTouchMove, false);
+		_domElement.removeEventListener('touchstart', onDocumentTouchStart, false);
+		_domElement.removeEventListener('touchend', onDocumentTouchEnd, false);
 
 	}
 
@@ -51,44 +52,44 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 	}
 
-	function onDocumentMouseMove( event ) {
+	function onDocumentMouseMove(event) {
 
 		event.preventDefault();
 
 		var rect = _domElement.getBoundingClientRect();
 
-		_mouse.x = ( ( event.clientX - rect.left ) / rect.width ) * 2 - 1;
-		_mouse.y = - ( ( event.clientY - rect.top ) / rect.height ) * 2 + 1;
+		_mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+		_mouse.y = - ((event.clientY - rect.top) / rect.height) * 2 + 1;
 
-		_raycaster.setFromCamera( _mouse, _camera );
+		_raycaster.setFromCamera(_mouse, _camera);
 
-		if ( _selected && scope.enabled ) {
+		if (_selected && scope.enabled) {
 
-			if ( _raycaster.ray.intersectPlane( _plane, _intersection ) ) {
+			if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
 
-				_selected.position.copy( _intersection.sub( _offset ).applyMatrix4( _inverseMatrix ) );
+				_selected.position.copy(_intersection.sub(_offset).applyMatrix4(_inverseMatrix));
 
 			}
 
-			scope.dispatchEvent( { type: 'drag', object: _selected } );
+			scope.dispatchEvent({ type: 'drag', object: _selected });
 
 			return;
 
 		}
 
-		_raycaster.setFromCamera( _mouse, _camera );
+		_raycaster.setFromCamera(_mouse, _camera);
 
-		var intersects = _raycaster.intersectObjects( _objects, true );
+		var intersects = _raycaster.intersectObjects(_objects, true);
 
-		if ( intersects.length > 0 ) {
+		if (intersects.length > 0) {
 
-			var object = intersects[ 0 ].object.parent;
+			var object = _getParentObjectByType(intersects[0].object, 'Group');
 
-			_plane.setFromNormalAndCoplanarPoint( _camera.getWorldDirection( _plane.normal ), _worldPosition.setFromMatrixPosition( object.matrixWorld ) );
+			_plane.setFromNormalAndCoplanarPoint(_camera.getWorldDirection(_plane.normal), _worldPosition.setFromMatrixPosition(object.matrixWorld));
 
-			if ( _hovered !== object ) {
+			if (_hovered !== object) {
 
-				scope.dispatchEvent( { type: 'hoveron', object: object } );
+				scope.dispatchEvent({ type: 'hoveron', object: object });
 
 				_domElement.style.cursor = 'pointer';
 				_hovered = object;
@@ -97,9 +98,9 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 		} else {
 
-			if ( _hovered !== null ) {
+			if (_hovered !== null) {
 
-				scope.dispatchEvent( { type: 'hoveroff', object: _hovered } );
+				scope.dispatchEvent({ type: 'hoveroff', object: _hovered });
 
 				_domElement.style.cursor = 'auto';
 				_hovered = null;
@@ -110,41 +111,41 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 	}
 
-	function onDocumentMouseDown( event ) {
+	function onDocumentMouseDown(event) {
 
 		event.preventDefault();
 
-		_raycaster.setFromCamera( _mouse, _camera );
+		_raycaster.setFromCamera(_mouse, _camera);
 
-		var intersects = _raycaster.intersectObjects( _objects, true );
+		var intersects = _raycaster.intersectObjects(_objects, true);
 
-		if ( intersects.length > 0 ) {
+		if (intersects.length > 0) {
 
-			_selected = intersects[ 0 ].object.parent;
+			_selected = _getParentObjectByType(intersects[0].object, 'Group');
 
-			if ( _raycaster.ray.intersectPlane( _plane, _intersection ) ) {
+			if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
 
-				_inverseMatrix.getInverse( _selected.parent.matrixWorld );
-				_offset.copy( _intersection ).sub( _worldPosition.setFromMatrixPosition( _selected.matrixWorld ) );
+				_inverseMatrix.getInverse(_selected.parent.matrixWorld);
+				_offset.copy(_intersection).sub(_worldPosition.setFromMatrixPosition(_selected.matrixWorld));
 
 			}
 
 			_domElement.style.cursor = 'move';
 
-			scope.dispatchEvent( { type: 'dragstart', object: _selected } );
+			scope.dispatchEvent({ type: 'dragstart', object: _selected });
 
 		}
 
 
 	}
 
-	function onDocumentMouseCancel( event ) {
+	function onDocumentMouseCancel(event) {
 
 		event.preventDefault();
 
-		if ( _selected ) {
+		if (_selected) {
 
-			scope.dispatchEvent( { type: 'dragend', object: _selected } );
+			scope.dispatchEvent({ type: 'dragend', object: _selected });
 
 			_selected = null;
 
@@ -154,27 +155,27 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 	}
 
-	function onDocumentTouchMove( event ) {
+	function onDocumentTouchMove(event) {
 
 		event.preventDefault();
-		event = event.changedTouches[ 0 ];
+		event = event.changedTouches[0];
 
 		var rect = _domElement.getBoundingClientRect();
 
-		_mouse.x = ( ( event.clientX - rect.left ) / rect.width ) * 2 - 1;
-		_mouse.y = - ( ( event.clientY - rect.top ) / rect.height ) * 2 + 1;
+		_mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+		_mouse.y = - ((event.clientY - rect.top) / rect.height) * 2 + 1;
 
-		_raycaster.setFromCamera( _mouse, _camera );
+		_raycaster.setFromCamera(_mouse, _camera);
 
-		if ( _selected && scope.enabled ) {
+		if (_selected && scope.enabled) {
 
-			if ( _raycaster.ray.intersectPlane( _plane, _intersection ) ) {
+			if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
 
-				_selected.position.copy( _intersection.sub( _offset ).applyMatrix4( _inverseMatrix ) );
+				_selected.position.copy(_intersection.sub(_offset).applyMatrix4(_inverseMatrix));
 
 			}
 
-			scope.dispatchEvent( { type: 'drag', object: _selected } );
+			scope.dispatchEvent({ type: 'drag', object: _selected });
 
 			return;
 
@@ -182,49 +183,49 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 	}
 
-	function onDocumentTouchStart( event ) {
+	function onDocumentTouchStart(event) {
 
 		event.preventDefault();
-		event = event.changedTouches[ 0 ];
+		event = event.changedTouches[0];
 
 		var rect = _domElement.getBoundingClientRect();
 
-		_mouse.x = ( ( event.clientX - rect.left ) / rect.width ) * 2 - 1;
-		_mouse.y = - ( ( event.clientY - rect.top ) / rect.height ) * 2 + 1;
+		_mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+		_mouse.y = - ((event.clientY - rect.top) / rect.height) * 2 + 1;
 
-		_raycaster.setFromCamera( _mouse, _camera );
+		_raycaster.setFromCamera(_mouse, _camera);
 
-		var intersects = _raycaster.intersectObjects( _objects, true );
+		var intersects = _raycaster.intersectObjects(_objects, true);
 
-		if ( intersects.length > 0 ) {
+		if (intersects.length > 0) {
 
-			_selected = intersects[ 0 ].object.parent;
+			_selected = _getParentObjectByType(intersects[0].object, 'Group');
 
-			_plane.setFromNormalAndCoplanarPoint( _camera.getWorldDirection( _plane.normal ), _worldPosition.setFromMatrixPosition( _selected.matrixWorld ) );
+			_plane.setFromNormalAndCoplanarPoint(_camera.getWorldDirection(_plane.normal), _worldPosition.setFromMatrixPosition(_selected.matrixWorld));
 
-			if ( _raycaster.ray.intersectPlane( _plane, _intersection ) ) {
+			if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
 
-				_inverseMatrix.getInverse( _selected.parent.matrixWorld );
-				_offset.copy( _intersection ).sub( _worldPosition.setFromMatrixPosition( _selected.matrixWorld ) );
+				_inverseMatrix.getInverse(_selected.parent.matrixWorld);
+				_offset.copy(_intersection).sub(_worldPosition.setFromMatrixPosition(_selected.matrixWorld));
 
 			}
 
 			_domElement.style.cursor = 'move';
 
-			scope.dispatchEvent( { type: 'dragstart', object: _selected } );
+			scope.dispatchEvent({ type: 'dragstart', object: _selected });
 
 		}
 
 
 	}
 
-	function onDocumentTouchEnd( event ) {
+	function onDocumentTouchEnd(event) {
 
 		event.preventDefault();
 
-		if ( _selected ) {
+		if (_selected) {
 
-			scope.dispatchEvent( { type: 'dragend', object: _selected } );
+			scope.dispatchEvent({ type: 'dragend', object: _selected });
 
 			_selected = null;
 
@@ -232,6 +233,17 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 		_domElement.style.cursor = 'auto';
 
+	}
+
+	function _getParentObjectByType(obj, type) {
+		while (obj.type != type) {
+			obj = obj.parent;
+			if (obj == null) {
+				return null;
+			}
+		} 
+
+		return obj;
 	}
 
 	activate();
@@ -246,5 +258,5 @@ THREE.DragControls = function ( _objects, _camera, _domElement ) {
 
 };
 
-THREE.DragControls.prototype = Object.create( THREE.EventDispatcher.prototype );
+THREE.DragControls.prototype = Object.create(THREE.EventDispatcher.prototype);
 THREE.DragControls.prototype.constructor = THREE.DragControls;
