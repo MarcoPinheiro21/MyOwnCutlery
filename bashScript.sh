7bd2ab14a3ee5fdcec6a45c8b07f790ba6cfe877
Carlos Moreira <1161882@isep.ipp.pt>
2020-01-04 16:00:57 +0000

    Correcção de bugs [ref issue #126]

--- bashScript.sh
+++ bashScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-open -a Docker
-docker ps -a 
-docker start `docker ps -q -l`
+# open -a Docker
+# docker ps -a 
+# docker start `docker ps -q -l`
 
 osascript -e 'tell application "Terminal" to activate
 tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
@@ -43,4 +43,11 @@ tell application "System Events" to tell process "Terminal" to keystroke "t" usi
 tell application "Terminal"
     delay 0.25
     do script "cd authApi && npm start" in front window
+end tell'
+
+osascript -e 'tell application "Terminal" to activate
+tell application "System Events" to tell process "Terminal" to keystroke "t" using command down
+tell application "Terminal"
+    delay 0.25
+    do script "cd algav && swipl -l productionplanning.pl -g server." in front window
 end tell'
